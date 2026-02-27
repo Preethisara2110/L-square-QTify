6abd40be8757b39e486a99e99fa3a3bf15edc9a2
preethi <preethisara2110gmail.com>
2026-02-27 21:32:25 +0530

    Fixed all ESLint errors for Netlify

--- src/components/Carousel/CarouselLeftNavigation/CarouselLeftNavigation.jsx
+++ src/components/Carousel/CarouselLeftNavigation/CarouselLeftNavigation.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, {  useState } from "react";
 import { useSwiper } from "swiper/react";
 import styles from "./CarouselLeftNavigation.module.css";
 import { ReactComponent as LeftArrow } from "../../../assets/LeftArrow.svg";
