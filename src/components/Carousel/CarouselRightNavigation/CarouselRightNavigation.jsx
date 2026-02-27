f7197dbe3c5d6af346a9a67aaed66faf777e034e
preethi <preethisara2110gmail.com>
2026-02-27 22:40:56 +0530

    Fix

--- src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
+++ src/components/Carousel/CarouselRightNavigation/CarouselRightNavigation.jsx
@@ -1,27 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useSwiper } from "swiper/react";
 import styles from "./CarouselRightNavigation.module.css";
 import { ReactComponent as RightArrow } from "../../../assets/RightArrow.svg";
 
 export default function CarouselRightNavigation() {
   const swiper = useSwiper();
-  const [isEnd, setIsEnd] = useState(swiper.isEnd);
-
-  useEffect(() => {
-    const handleSlideChange = () => {
-      setIsEnd(swiper.isEnd);
-    };
-
-    swiper.on("slideChange", handleSlideChange);
-
-    return () => {
-      swiper.off("slideChange", handleSlideChange);
-    };
-  }, [swiper]);
 
   return (
     <div className={styles.rightNavigation}>
-      {!isEnd && <RightArrow onClick={() => swiper.slideNext()} />}
+      {!swiper.isEnd && (
+        <RightArrow onClick={() => swiper.slideNext()} />
+      )}
     </div>
   );
 }
